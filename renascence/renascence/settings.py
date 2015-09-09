6f02f2e44a1885dddad565a145d7641b9fa8b933
Tom Lin <tao.lin3@hp.com>
2015-09-10 02:29:07 +0800

    Add grappelli.

--- renascence/renascence/settings.py
+++ renascence/renascence/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+    'grappelli',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -112,3 +113,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+GRAPPELLI_ADMIN_TITLE = "Renascence"
--- renascence/renascence/urls.py
+++ renascence/renascence/urls.py
@@ -19,5 +19,6 @@ from django.contrib import admin
 admin.site.site_header = 'Renascence'
 
 urlpatterns = [
+    url(r'^grappelli/', include('grappelli.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
